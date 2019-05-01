f632045937db2bbdf2cbf626dde647a6059c2cdc
Charles Burch <georgia_beast89@yahoo.com>
2019-04-30 20:25:24 -0500

    updated the package.json and create the event dashboard component

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,12 +1,12 @@
 import React,{Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { Button } from "semantic-ui-react";
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard'
 class App extends Component {
   render(){
      return (
     <div>
       <h1>Eventer</h1>
+      <EventDashboard></EventDashboard>
     </div>
   ); 
   }
