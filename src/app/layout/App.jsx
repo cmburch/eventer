b9e22a4c0d94832251adfa8763b54d2d2b58a78d
Charles Burch <georgia_beast89@yahoo.com>
2019-04-30 21:23:01 -0500

    added the navigation bar and some default styles to the react app

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,12 +1,15 @@
 import React,{Component} from 'react';
-import { Button } from "semantic-ui-react";
+import { Container } from "semantic-ui-react";
 import EventDashboard from '../../features/event/EventDashboard/EventDashboard'
+import NavBar from '../../features/nav/NavBar/NavBar'
 class App extends Component {
   render(){
      return (
     <div>
-      <h1>Eventer</h1>
-      <EventDashboard></EventDashboard>
+      <NavBar></NavBar>
+      <Container className="main">
+        <EventDashboard></EventDashboard>
+      </Container>
     </div>
   ); 
   }
