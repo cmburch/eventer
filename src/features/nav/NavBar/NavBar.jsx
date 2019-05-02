65cdd87a5e3e039dba325f83a68fbcc04d2040a3
Charles Burch <georgia_beast89@yahoo.com>
2019-05-02 13:23:28 -0500

    used the withRouter with navbar to signOut and navigate back to home page

--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Menu,Button,Container } from 'semantic-ui-react'
-import { NavLink,Link } from 'react-router-dom';
+import { NavLink,Link,withRouter } from 'react-router-dom';
 import SignedOutMenu from '../Menus/SignedOutMenu';
 import SignedInMenu from '../Menus/SignedInMenu';
 
@@ -19,6 +19,7 @@ class NavBar extends Component {
     this.setState({
       authenticated: false
     })
+    this.props.history.push('/')
   }
   render() {
     const {authenticated} = this.state;
@@ -30,10 +31,12 @@ class NavBar extends Component {
                     Eventer
                   </Menu.Item>
                   <Menu.Item as={NavLink} to='/events' name="Events" />
-                  <Menu.Item as={NavLink} to='/people' name="People" />
+                  {authenticated &&
+                   <Menu.Item as={NavLink} to='/people' name="People" />}
+                  {authenticated &&
                   <Menu.Item>
                     <Button as={Link} to='/createEvent' floated="right" positive inverted content="Create Event" />
-                  </Menu.Item>
+                  </Menu.Item>}
                   {authenticated ? <SignedInMenu signOut={this.handleSignOut}/> : <SignedOutMenu signIn={this.handleSignIn}/>}                  
                 </Container>
               </Menu>
@@ -41,4 +44,4 @@ class NavBar extends Component {
   }
 }
 
-export default NavBar
+export default withRouter(NavBar);
