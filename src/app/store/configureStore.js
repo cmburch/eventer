49078f6141ae4961d3da044a5a5408a8f7ab741b
Charles Burch <georgia_beast89@yahoo.com>
2019-05-02 17:37:01 -0500

    created the first test reducers

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware,compose } from "redux";
-
+import rootReducer from '../reducers/rootReducer';
 
 export const configureStore =(preloadState)=> {
     const middlewares = [];
