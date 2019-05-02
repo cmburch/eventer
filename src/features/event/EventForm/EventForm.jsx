520cb77f1c3b1b569a2c1b428457cca1140a3a4e
Charles Burch <georgia_beast89@yahoo.com>
2019-05-01 19:02:17 -0500

    modified event Form when a event is selected the form is intialize,but still needs work

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -1,16 +1,25 @@
 import React, { Component } from 'react'
 import { Segment,Form,Button,List} from 'semantic-ui-react';
 
+const emptyEvent = {
+  title: '',
+  date: '',
+  city: '',
+  venue: '',
+  hostedBy: '',
+}
 class EventForm extends Component {
     state = {
-        event: {
-            title: '',
-            date: '',
-            city: '',
-            venue: '',
-            hostedBy: '',
-        }
+        event: emptyEvent
     }
+    componentDidMount() {
+      if(this.props.selectedEvent !== null){
+        this.setState({
+          event: this.props.selectedEvent
+      });
+      }
+    }
+
     onFormSubmit = (evt)=> {
         evt.preventDefault();
         this.props.createEvent(this.state.event);
