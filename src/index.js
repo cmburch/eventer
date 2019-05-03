7d6473c5529afbbeb58dc70216a310e7d07133e5
Charles Burch <georgia_beast89@yahoo.com>
2019-05-02 21:06:50 -0500

    modified the package.json to fix errors

--- src/index.js
+++ src/index.js
@@ -11,10 +11,9 @@ import * as serviceWorker from './serviceWorker';
 // const store = configureStore();
 
 ReactDOM.render(
-    // 
-    <BrowserRouter>
+     <BrowserRouter>
         <App />
-    </BrowserRouter>
+     </BrowserRouter>  
 , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
